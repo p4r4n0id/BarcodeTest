42ccd06126ce43a5f3b96433173947d2aa1f1cd3
p4r4n0id <kai.donato@web.de>
2014-03-24 20:24:40 +0100

    123
    123

--- js/index.js
+++ js/index.js
@@ -58,16 +58,22 @@ var app = {
 
         scanner.scan( function (result) { 
 
-            alert("We got a barcode\n" + 
+        	var erg = "Ergebnis "+result.text+"\n Format: "+result.format;
+        	document.getElementById("result_code").innerHTML = erg;
+            /*alert("We got a barcode\n" + 
             "Result: " + result.text + "\n" + 
             "Format: " + result.format + "\n" + 
             "Cancelled: " + result.cancelled);  
+        */
 
            console.log("Scanner result: \n" +
                 "text: " + result.text + "\n" +
                 "format: " + result.format + "\n" +
                 "cancelled: " + result.cancelled + "\n");
-            document.getElementById("info").innerHTML = result.text;
+            window.plugins.ChildBrowser.showWebPage('https://apex.mt-ag.com/pls/apex/f?p=205:2:0::::P2_PDT_EAN:'+result.text, { showLocationBar: true });
+             //window.location = 'https://apex.mt-ag.com/pls/apex/f?p=205:2:0::::P2_PDT_EAN:'+result.text;
+            //window.location.href = "https://apex.mt-ag.com/pls/apex/f?p=205:2:0::::P2_PDT_EAN:"+result.text;
+            //document.getElementById("info").innerHTML = result.text;
             console.log(result);
             /*
             if (args.format == "QR_CODE") {
